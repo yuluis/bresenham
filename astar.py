4af98c84acac409b1a37e50ee55093df2729d3ef
yuluis <yuluis@gmail.com>
2018-03-05 19:18:48 -0800

    Add existing file

---
+++ astar.py
@@ -0,0 +1,148 @@
+from queue import PriorityQueue
+import numpy as np
+from enum import Enum
+
+
+class Action(Enum):
+    """
+    An action is represented by a 3 element tuple.
+
+    The first 2 values are the delta of the action relative
+    to the current grid position. The third and final value
+    is the cost of performing the action.
+    """
+    LEFT = (0, -1, 1)
+    RIGHT = (0, 1, 1)
+    UP = (-1, 0, 1)
+    DOWN = (1, 0, 1)
+
+    def __str__(self):
+        if self == self.LEFT:
+            return '<'
+        elif self == self.RIGHT:
+            return '>'
+        elif self == self.UP:
+            return '^'
+        elif self == self.DOWN:
+            return 'v'
+
+    @property
+    def cost(self):
+        return self.value[2]
+
+    @property
+    def delta(self):
+        return (self.value[0], self.value[1])
+
+
+def valid_actions(grid, current_node):
+    """
+    Returns a list of valid actions given a grid and current node.
+    """
+    valid = [Action.UP, Action.LEFT, Action.RIGHT, Action.DOWN]
+    n, m = grid.shape[0] - 1, grid.shape[1] - 1
+    x, y = current_node
+
+    # check if the node is off the grid or
+    # it's an obstacle
+
+    if x - 1 < 0 or grid[x - 1, y] == 1:
+        valid.remove(Action.UP)
+    if x + 1 > n or grid[x + 1, y] == 1:
+        valid.remove(Action.DOWN)
+    if y - 1 < 0 or grid[x, y - 1] == 1:
+        valid.remove(Action.LEFT)
+    if y + 1 > m or grid[x, y + 1] == 1:
+        valid.remove(Action.RIGHT)
+
+    return valid
+
+
+def visualize_path(grid, path, start):
+    sgrid = np.zeros(np.shape(grid), dtype=np.str)
+    sgrid[:] = ' '
+    sgrid[grid[:] == 1] = 'O'
+
+    pos = start
+
+    for a in path:
+        da = a.value
+        sgrid[pos[0], pos[1]] = str(a)
+        pos = (pos[0] + da[0], pos[1] + da[1])
+    sgrid[pos[0], pos[1]] = 'G'
+    sgrid[start[0], start[1]] = 'S'
+    return sgrid
+
+# TODO: implement a heuristic function. This may be one of the
+# functions described above or feel free something to think of something
+# else.
+def heuristic(position, goal_position):
+    h = np.sqrt((goal_position[0]-position[0])^2 + (goal_position[1]-position[1])^2)
+    return h
+
+
+def a_star(grid, h, start, goal):
+    path = []
+    queue = PriorityQueue()
+    queue.put((0, start))
+    visited = set(start)
+
+    branch = {}
+    found = False
+
+    while not queue.empty():
+        item = queue.get()
+        current_cost = item[0]
+        current_node = item[1]
+
+        if current_node == goal:
+            print('Found a path.')
+            found = True
+            break
+        else:
+            for action in valid_actions(grid, current_node):
+                # get the tuple representation
+                da = action.delta
+                cost = action.cost
+                next_node = (current_node[0] + da[0], current_node[1] + da[1])
+                # TODO: calculate new cost, c + g() + h()
+                new_cost = current_cost + cost + heuristic(current_node, goal)
+
+                if next_node not in visited:
+                    visited.add(next_node)
+                    queue.put((new_cost, next_node))
+
+                    branch[next_node] = (new_cost, current_node, action)
+
+    path = []
+    path_cost = 0
+    if found:
+
+        # retrace steps
+        path = []
+        n = goal
+        path_cost = branch[n][0]
+        while branch[n][1] != start:
+            path.append(branch[n][2])
+            n = branch[n][1]
+        path.append(branch[n][2])
+
+    return path[::-1], path_cost
+
+start = (0, 0)
+goal = (4, 4)
+
+grid = np.array([
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0],
+    [0, 0, 0, 1, 0, 0],
+])
+
+path, cost = a_star(grid, heuristic, start, goal)
+print(path, cost)
+
+# S -> start, G -> goal, O -> obstacle
+print(visualize_path(grid, path, start))
+
